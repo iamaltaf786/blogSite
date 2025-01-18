9e4e410e35727f565d8475918a49954abc0235b0
Altaf Hussain <76874013+iamaltaf786@users.noreply.github.com>
2025-01-18 19:05:44 +0530

    modified app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import { Homepage, BlogContentPage } from "./pages";
-import { Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import useFetch from "./hooks/useFetch";
 
 // ?populate=* this in url is formatting tools to show
@@ -15,13 +15,18 @@ function App() {
 
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Homepage blogs={data ? data : ""} />}></Route>
-        <Route
-          path="/blog/:id"
-          element={<BlogContentPage blogs={data ? data : ""} />}
-        ></Route>
-      </Routes>
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={<Homepage blogs={data ? data : ""} />}
+          ></Route>
+          <Route
+            path="/blog/:id"
+            element={<BlogContentPage blogs={data ? data : ""} />}
+          ></Route>
+        </Routes>
+      </Router>
     </>
   );
 }
