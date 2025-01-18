34c6e7223ead8de9811f4c1c697cbec3c3c5dbfb
Altaf Hussain <76874013+iamaltaf786@users.noreply.github.com>
2025-01-18 19:28:47 +0530

    modified app and main .jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
 import "./App.css";
 import { Homepage, BlogContentPage } from "./pages";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import useFetch from "./hooks/useFetch";
-
-// ?populate=* this in url is formatting tools to show
-// APIs in correct format.
 
 function App() {
   let { loading, data, error } = useFetch(
@@ -14,20 +11,13 @@ function App() {
   if (error) return <p>Error!</p>;
 
   return (
-    <>
-      <Router>
-        <Routes>
-          <Route
-            path="/"
-            element={<Homepage blogs={data ? data : ""} />}
-          ></Route>
-          <Route
-            path="/blog/:id"
-            element={<BlogContentPage blogs={data ? data : ""} />}
-          ></Route>
-        </Routes>
-      </Router>
-    </>
+    <Routes>
+      <Route path="/" element={<Homepage blogs={data ? data : ""} />}></Route>
+      <Route
+        path="/blog/:id"
+        element={<BlogContentPage blogs={data ? data : ""} />}
+      ></Route>
+    </Routes>
   );
 }
 
